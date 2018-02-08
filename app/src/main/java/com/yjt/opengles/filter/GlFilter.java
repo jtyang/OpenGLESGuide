2df53756e383559de7da3ba50140eaee5b0eb8b6
yangjiantong <yangjiantong1988@126.com>
2018-02-08 12:24:06 +0800

    add GlGroupFilter

--- app/src/main/java/com/yjt/opengles/filter/GlFilter.java
+++ app/src/main/java/com/yjt/opengles/filter/GlFilter.java
@@ -44,7 +44,7 @@ public class GlFilter {
 
     private int program;
     private int textureID = -12345;
-    protected float[] clearColor = new float[]{0f, 0f, 0f, 1f};
+    private float[] clearColor = new float[]{0f, 0f, 0f, 0f};
 
     private final HashMap<String, Integer> handleMap = new HashMap<>();
 
@@ -73,7 +73,7 @@ public class GlFilter {
     }
 
 
-    public void draw(SurfaceTexture surfaceTexture, float[] STMatrix, float[] MVPMatrix) {
+    public void drawOES(SurfaceTexture surfaceTexture, float[] STMatrix, float[] MVPMatrix) {
         GlUtils.checkGlError("onDrawFrame start");
 
 
@@ -196,5 +196,70 @@ public class GlFilter {
         this.clearColor = new float[]{red, green, blue, alpha};
     }
 
+
+    protected int viewWidth, viewHeight;
+
+    public void onInit(int vWidth, int vHeight) {
+        this.viewWidth = vWidth;
+        this.viewHeight = vHeight;
+        final int vertexShader = GlUtils.loadShader(vertexShaderSource, GLES20.GL_VERTEX_SHADER);
+        final int fragmentShader = GlUtils.loadShader(fragmentShaderSource, GLES20.GL_FRAGMENT_SHADER);
+        program = GlUtils.createProgram(vertexShader, fragmentShader);
+        if (program == 0) {
+            throw new RuntimeException("failed creating program");
+        }
+
+        getAttribHandle("aPosition");
+        getAttribHandle("aTextureCoord");
+        getUniformHandle("uMVPMatrix");
+        getUniformHandle("uSTMatrix");
+    }
+
+    public void drawTexture2D(int cameraTexture, int targetFrameBuffer, float[] mvpMatrix, float[] stMatrix) {
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, targetFrameBuffer);
+
+        GLES20.glClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3]);
+        GLES20.glClear(GLES20.GL_DEPTH_BUFFER_BIT | GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glUseProgram(program);
+        GlUtils.checkGlError("glUseProgram");
+
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, cameraTexture);
+
+        GLES20.glEnableVertexAttribArray(getHandle("aPosition"));
+        GlUtils.checkGlError("glEnableVertexAttribArray aTextureHandle");
+        triangleVertices.position(TRIANGLE_VERTICES_DATA_POS_OFFSET);
+        GLES20.glVertexAttribPointer(getHandle("aPosition"), 3, GLES20.GL_FLOAT, false,
+                TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangleVertices);
+
+        GLES20.glEnableVertexAttribArray(getHandle("aTextureCoord"));
+        GlUtils.checkGlError("glEnableVertexAttribArray aTextureHandle");
+        triangleVertices.position(TRIANGLE_VERTICES_DATA_UV_OFFSET);
+        GLES20.glVertexAttribPointer(getHandle("aTextureCoord"), 2, GLES20.GL_FLOAT, false,
+                TRIANGLE_VERTICES_DATA_STRIDE_BYTES, triangleVertices);
+        GlUtils.checkGlError("glVertexAttribPointer aTextureHandle");
+
+
+        GLES20.glUniformMatrix4fv(getHandle("uMVPMatrix"), 1, false, mvpMatrix, 0);
+        GLES20.glUniformMatrix4fv(getHandle("uSTMatrix"), 1, false, stMatrix, 0);
+
+        GLES20.glViewport(0, 0, viewWidth, viewHeight);
+
+        //pre draw
+        onDraw();
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+        GlUtils.checkGlError("glDrawArrays");
+        GLES20.glFinish();
+
+        //after draw
+
+        GLES20.glDisableVertexAttribArray(getHandle("aPosition"));
+        GLES20.glDisableVertexAttribArray(getHandle("aTextureCoord"));
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        GLES20.glUseProgram(0);
+
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, 0);
+    }
 
 }
--- app/src/main/java/com/yjt/opengles/filter/GlGroupFilter.java
+++ app/src/main/java/com/yjt/opengles/filter/GlGroupFilter.java
@@ -1,11 +1,92 @@
 package com.yjt.opengles.filter;
 
+import android.opengl.GLES20;
+
+import com.yjt.opengles.utils.GlUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * 文件描述:
+ * 滤镜组
  *
  * @author yangjiantong
  * @date 2018/2/7
  */
-public class GlGroupFilter {
+public class GlGroupFilter extends GlFilter {
+
+    private LinkedList<FilterWrapper> filterWrappers;
+
+    public GlGroupFilter(List<GlFilter> filters) {
+        if (filters == null || filters.isEmpty()) {
+            throw new IllegalArgumentException("can not create empty GroupFilter");
+        }
+        filterWrappers = new LinkedList<>();
+        for (GlFilter filter : filters) {
+            filterWrappers.add(new FilterWrapper(filter));
+        }
+    }
+
+    @Override
+    public void onInit(int vWidth, int vHeight) {
+        super.onInit(vWidth, vHeight);
+        for (FilterWrapper wrapper : filterWrappers) {
+            wrapper.filter.onInit(vWidth, vHeight);
+            int[] frameBuffer = new int[1];
+            int[] frameBufferTexture = new int[1];
+            GlUtils.createFrameBuff(frameBuffer,
+                    frameBufferTexture,
+                    viewWidth,
+                    viewHeight);
+            wrapper.frameBuffer = frameBuffer[0];
+            wrapper.frameBufferTexture = frameBufferTexture[0];
+        }
+    }
+
+
+    @Override
+    public void drawTexture2D(int cameraTexture, int targetFrameBuffer, float[] mvpMatrix, float[] stMatrix) {
+        FilterWrapper preFilterWrapper = null;
+        int i = 0;
+        int texture;
+        for (FilterWrapper wrapper : filterWrappers) {
+            if (preFilterWrapper == null) {
+                texture = cameraTexture;
+            } else {
+                texture = preFilterWrapper.frameBufferTexture;
+            }
+            if (i == (filterWrappers.size() - 1)) {
+                wrapper.filter.drawTexture2D(texture, targetFrameBuffer, mvpMatrix, stMatrix);
+            } else {
+                wrapper.filter.drawTexture2D(texture, wrapper.frameBuffer, mvpMatrix, stMatrix);
+            }
+            preFilterWrapper = wrapper;
+            i++;
+        }
+    }
+
+
+    @Override
+    public void release() {
+        super.release();
+        if (filterWrappers != null && filterWrappers.size() > 0) {
+            for (FilterWrapper wrapper : filterWrappers) {
+                wrapper.filter.release();
+                GLES20.glDeleteFramebuffers(1, new int[]{wrapper.frameBuffer}, 0);
+                GLES20.glDeleteTextures(1, new int[]{wrapper.frameBufferTexture}, 0);
+            }
+            filterWrappers.clear();
+        }
+    }
+
+    private class FilterWrapper {
+        GlFilter filter;
+        int frameBuffer;
+        int frameBufferTexture;
+
+        FilterWrapper(GlFilter filter) {
+            this.filter = filter;
+        }
+    }
 
 }
